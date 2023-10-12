5a279435c0709eff5162e62e5fd159fd69d52c49
Imtiaz <im3azchowdhury@gmail.com>
2023-10-12 23:19:30 +0600

    structure created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSCCBorksoYz9iwMUeYp7Ufvk36qKwuFg",
+  authDomain: "health-fits.firebaseapp.com",
+  projectId: "health-fits",
+  storageBucket: "health-fits.appspot.com",
+  messagingSenderId: "112617174775",
+  appId: "1:112617174775:web:63e2d8fee5bb5049f0c5f2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
+ 
